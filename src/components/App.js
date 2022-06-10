b25527bd329581e54816a4e5ebb06262abd52f78
MargaritaBusyginaCa <busygina.rita.ca@gmail.com>
2022-06-09 21:12:37 -0700

    displaying different recipies from form imput

--- src/components/App.js
+++ src/components/App.js
@@ -2,27 +2,56 @@ import React, {useState, useEffect} from "react"
 import {Link, Routes, Route} from 'react-router-dom'
 function App(){
     const [recipes, setRecipes] = useState([])
-    const [randomRecipes, setRandomRecipes] = useState([])
+    const [formData, setFormData] =useState("")
+    const [queryNum, setQueryNum] = useState(10)
     const [load, setLoad] = useState(false)
-    //6b0b1262b4284a6bb7255205dff10a83
+    let apiKey = "6b0b1262b4284a6bb7255205dff10a83"
+    //https://api.spoonacular.com/recipes/654959/information?apiKey=6b0b1262b4284a6bb7255205dff10a83"
+    
+    let url = `https://api.spoonacular.com/recipes/complexSearch?query=${formData}&apiKey=${apiKey}&number=${queryNum}`
     useEffect(() =>{
-      fetch('https://api.spoonacular.com/recipes/complexSearch?query=pasta&apiKey=6b0b1262b4284a6bb7255205dff10a83&number=1')
+      fetch(url)
       .then(res => res.json())
       .then(data => setRecipes(data.results))
-       
-    }, load)
-    console.log(recipes)
+      console.log(recipes)
+    }, [load])
+
+    function handleOnChange(e){
+        e.preventDefault()
+        setFormData(e.target.value)
+        
+    }
+    function findRecipes(){
+        setLoad(prevLoad => !prevLoad)
+        console.log(load)
+    }
+    let recipesDisplay = recipes.map(r =>{
+        return(
+            <div key={r.id}>
+                <h2>{r.title}</h2>
+                <img src={r.image}/>
+            </div>
+        )
+    })
     return(
         <div>
             <div>
               <form>
-                
+                <input type="text"
+                       value={formData}
+                       autoComplete="off"
+                       name="recipe"
+                       placeholder="Search for new recipes"
+                       onChange={handleOnChange}
+                />
+                <button type="button" onClick={findRecipes} className="submit-btn"></button>
               </form>
+              {recipesDisplay}
             </div>
 
-            <Routes>
+            {/* <Routes>
                 <Route path="/:recipeId"></Route>
-            </Routes>
+            </Routes> */}
         </div>
     )
 }
